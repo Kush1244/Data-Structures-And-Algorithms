c1ed02f8f55bd603aef8a11b5a88bd39303fdf73
Kush1244 <kushaagraverma@gmail.com>
2023-12-22 23:45:59 +0530

    Fromat the code with google style guidelines

--- ds_impl/static_array.hpp
+++ ds_impl/static_array.hpp
@@ -1,49 +1,37 @@
 #ifndef STATIC_ARRAY_HPP
 #define STATIC_ARRAY_HPP 1
 
-#include "../config.hpp"
 #include <cstdlib>
 #include <iostream>
-NS_BEGIN( my )
 
+#include "../config.hpp"
+NS_BEGIN(my)
 
 template <typename T>
-class iterator
-{
-  public:
-    explicit iterator( T *ptr ) : m_Ptr( ptr )
-    {
-    }
+class iterator {
+ public:
+  explicit iterator(T *ptr) : m_Ptr(ptr) {}
 
-    iterator &operator++( int )
-    {
-        m_Ptr++;
-        return *this;
-    }
+  iterator &operator++(int) {
+    m_Ptr++;
+    return *this;
+  }
 
-    iterator &operator++()
-    {
-        m_Ptr++;
-        return *this;
-    }
+  iterator &operator++() {
+    m_Ptr++;
+    return *this;
+  }
 
-    T &operator*()
-    {
-        return *m_Ptr;
-    }
-    T *&get_pointer()
-    {
-        return m_Ptr;
-    }
+  T &operator*() { return *m_Ptr; }
+  T *&get_pointer() { return m_Ptr; }
 
-  private:
-    T *m_Ptr;
+ private:
+  T *m_Ptr;
 };
 
 template <typename T>
-bool operator!=( iterator<T> &first, iterator<T> &second )
-{
-    return first.get_pointer() != second.get_pointer();
+bool operator!=(iterator<T> &first, iterator<T> &second) {
+  return first.get_pointer() != second.get_pointer();
 }
 
 /**
@@ -53,28 +41,27 @@ bool operator!=( iterator<T> &first, iterator<T> &second )
  * @tparam N Fix Size Length (Known at compile Time)
  */
 template <class T, my::size_t N>
-class static_array
-{
-  public:
-    static_array();
-    explicit static_array( T t_initVal );
-    my::size_t size();
-    T &operator[]( my::size_t index );
-    T &at( my::size_t index );
-    T *&data();
-    bool is_empty();
-    void fill( T fillValue );
-    iterator<T> begin();
-    iterator<T> end();
+class static_array {
+ public:
+  static_array();
+  explicit static_array(T t_initVal);
+  my::size_t size();
+  T &operator[](my::size_t index);
+  T &at(my::size_t index);
+  T *&data();
+  bool is_empty();
+  void fill(T fillValue);
+  iterator<T> begin();
+  iterator<T> end();
 
-  private:
-    T mArr[N];
+ private:
+  T mArr[N];
 };
 
-NS_END( my )
+NS_END(my)
 
 template <class T, my::size_t N>
-std::ostream &operator<<( std::ostream &os, my::static_array<T, N> &arr );
+std::ostream &operator<<(std::ostream &os, my::static_array<T, N> &arr);
 
 /////////// IMPLEMENTATION ///////////////
 
@@ -82,79 +69,64 @@ template <class T, my::size_t N>
 my::static_array<T, N>::static_array() = default;
 
 template <class T, my::size_t N>
-my::static_array<T, N>::static_array( T t_initVal )
-{
-    for ( my::size_t i = 0; i < N; i++ )
-    {
-        mArr[i] = t_initVal;
-    }
+my::static_array<T, N>::static_array(T t_initVal) {
+  for (my::size_t i = 0; i < N; i++) {
+    mArr[i] = t_initVal;
+  }
 }
 template <class T, my::size_t N>
-my::size_t my::static_array<T, N>::size()
-{
-    return N;
+my::size_t my::static_array<T, N>::size() {
+  return N;
 }
 
 template <class T, my::size_t N>
-T &my::static_array<T, N>::operator[]( my::size_t index )
-{
-    return mArr[index];
+T &my::static_array<T, N>::operator[](my::size_t index) {
+  return mArr[index];
 }
 
 template <class T, my::size_t N>
-T &my::static_array<T, N>::at( my::size_t index )
-{
-    if ( index >= N )
-    {
-        std::cout << "Index out of range" << std::endl; // TO REMOVE
-        exit( 1 ); // TODO assertion "access out of range"
-    }
-    return mArr[index];
-}
-
-
-template <class T, my::size_t N>
-T *&my::static_array<T, N>::data()
-{
-    return mArr;
+T &my::static_array<T, N>::at(my::size_t index) {
+  if (index >= N) {
+    std::cout << "Index out of range" << std::endl;  // TO REMOVE
+    exit(1);  // TODO assertion "access out of range"
+  }
+  return mArr[index];
 }
 
 template <class T, my::size_t N>
-bool my::static_array<T, N>::is_empty()
-{
-    return N == 0;
+T *&my::static_array<T, N>::data() {
+  return mArr;
 }
 
 template <class T, my::size_t N>
-void my::static_array<T, N>::fill( T fillValue )
-{
-    for ( my::size_t i = 0; i < N; i++ )
-    {
-        mArr[i] = fillValue;
-    }
+bool my::static_array<T, N>::is_empty() {
+  return N == 0;
 }
-//////////////////////////// std::cout implementation ///////////////
+
 template <class T, my::size_t N>
-std::ostream &operator<<( std::ostream &os, my::static_array<T, N> &arr )
-{
-    for ( my::size_t i = 0; i < N; i++ )
-    {
-        std::cout << arr[i] << " ";
-    }
-    return os;
+void my::static_array<T, N>::fill(T fillValue) {
+  for (my::size_t i = 0; i < N; i++) {
+    mArr[i] = fillValue;
+  }
+}
+//////////////////////////// std::ostream overloading ///////////////
+template <class T, my::size_t N>
+std::ostream &operator<<(std::ostream &os, my::static_array<T, N> &arr) {
+  for (my::size_t i = 0; i < N; i++) {
+    std::cout << arr[i] << " ";
+  }
+  return os;
 }
 
 //////////////////////////////// Iterator Implementation ////////////////////
 template <class T, my::size_t N>
-my::iterator<T> my::static_array<T, N>::begin()
-{
-    return my::iterator<T>( mArr );
+my::iterator<T> my::static_array<T, N>::begin() {
+  return my::iterator<T>(mArr);
 }
 
 template <class T, my::size_t N>
-my::iterator<T> my::static_array<T, N>::end()
-{
-    return my::iterator<T>( mArr + N );
+my::iterator<T> my::static_array<T, N>::end() {
+  return my::iterator<T>(mArr + N);
 }
 
-#endif // STATIC_ARRAY_HPP
+#endif  // STATIC_ARRAY_HPP
